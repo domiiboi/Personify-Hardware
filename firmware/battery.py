d30731e66ded6bf21e1f410e16d11a9007190641
ShawnTotaram <71612518+ShawnTotaram@users.noreply.github.com>
2021-03-05 09:20:23 -0500

    Update battery.py

--- firmware/battery.py
+++ firmware/battery.py
@@ -1,57 +1,41 @@
-#imports
-from vcgencmd import Vcgencmd
-import pyrebase
-import time
+# Raspberry Pi LM75A I2C temperature sample code.
+# Author: Leon Anavi <leon@anavi.org>
 import requests
+import sys
+import smbus
+import time
 import json
 
-#firebase
-config = {
-  "apiKey": "AIzaSyC42ryHyiziZbOVNHCNxmCso0_vGt0bW90",
-  "authDomain": "voltagereaderpersonify.firebaseapp.com",
-  "databaseURL": "https://voltagereaderpersonify-default-rtdb.firebaseio.com",
-  "storageBucket": "voltagereaderpersonify.appspot.com"
-  
-}
+# By default the address of LM75A is set to 0x48
+# aka A0, A1, and A2 are set to GND (0v).
+address = 0x48
 
-firebase = pyrebase.initialize_app(config)
+# Check if another address has been specified
+if 1 < len(sys.argv):
+	address = int(sys.argv[1], 16)
 
-db = firebase.database()
-#loop 5 second increments
-
-while True:
-  #function call to read core coltage
-    vcgm = Vcgencmd()
-    output = vcgm.measure_volts("core")
-
-    
-    
-   #stores variable in output
-    print(output)
-   
-   #stores output in data under core voltage in firebase
-    data = {"corevoltage": output}
-
-    #creates child under core voltage and stores data under reading
-    db.child("reading").push(data)
-
-
-    url = "https://us-central1-personify-c98fc.cloudfunctions.net/postDeviceData"
-    data = {
+# Print temperature
+while True:	
+# Read I2C data and calculate temperature
+  bus = smbus.SMBus(1)
+  raw = bus.read_word_data(address, 0) & 0xFFFF
+  raw = ((raw << 8) & 0xFF00) + (raw >> 8)
+  temperature = (raw / 32.0) / 8.0
+  #Push Data
+  data={"temp":temperature}
+  url = "https://us-central1-personify-c98fc.cloudfunctions.net/postDeviceData"
+  data = {
         "uid": "XHVfPr23F7Meg0QuwEkYFvI8Fqd2",
         "accessCode": "apple",
         "DATA": {
-        "value": output,
-        "type": "CORE_VOLTAGE"
+        "value": temperature,
+        "type": "TEMP"
         }
     }
 
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r = requests.post(url, data=json.dumps(data), headers=headers)
+  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+  r = requests.post(url, data=json.dumps(data), headers=headers)
 
-
-    print(r.text)
-
-
-    time.sleep(10)
-
+  print(r.text)
+  
+  time.sleep(3)
