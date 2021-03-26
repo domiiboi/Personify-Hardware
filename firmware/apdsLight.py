ba783339a0ed4ab3914f62f8d4774d67d8503546
Philbert <55509062+philbertFont@users.noreply.github.com>
2021-03-26 09:46:43 -0400

    Update apdsLight.py

--- firmware/apdsLight.py
+++ firmware/apdsLight.py
@@ -3,6 +3,13 @@ from apds9960 import APDS9960
 import smbus
 from time import sleep
 import pyrebase
+import requests,json
+
+
+url = 'https://us-central1-personify-c98fc.cloudfunctions.net/postDeviceData'
+
+
+
 
 config = {
   "apiKey": " ",
@@ -19,17 +26,31 @@ bus = smbus.SMBus(port)
 apds = APDS9960(bus)
 
 try:
-    print("Light Sensor")
-    print("============")
-    apds.enableLightSensor()    
-    oval = -1
-    
-    while True:
-        sleep(2)
-        val = apds.readAmbientLight()
-        if val != oval:
-		print("Light Detected={}".format(val))
-        	db.child("data").update({"light":val})
-		oval = val
+ print("Light Sensor")
+ print("============")
+ apds.enableLightSensor()
+ oval = -1
+
+ while True:
+  sleep(10)
+  val = apds.readAmbientLight()
+  if val != oval:
+   print("Light Detected={}".format(val))
+
+
+   data= {"uid":'XHVfPr23F7Meg0QuwEkYFvI8Fqd2',"accessCode":"apple",
+          "DATA":{'value':val,'type':"LIGHT_SENSOR"}
+         }
+   headers = {'Content-type':'application/json','Accept':'text/plain'}
+
+   x = requests.post(url, data=json.dumps(data), headers=headers)
+
+   print(x.text)
+
+
+   db.child("data").update({"light":val})
+   oval = val
 finally:
     print("Bye")
+    apds.disableLightSensor()
+
