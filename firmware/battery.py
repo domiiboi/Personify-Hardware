a69346c0fdd18ef71db473d01c248add6e809b4d
Sameer M. Khan <55508200+sameermkhan-creator@users.noreply.github.com>
2021-04-16 08:27:42 -0300

    Update battery.py

--- firmware/battery.py
+++ firmware/battery.py
@@ -1,41 +1,39 @@
-# Raspberry Pi LM75A I2C temperature sample code.
-# Author: Leon Anavi <leon@anavi.org>
-import requests
-import sys
-import smbus
+#imports
+from vcgencmd import Vcgencmd
+import pyrebase
 import time
+import requests
 import json
 
-# By default the address of LM75A is set to 0x48
-# aka A0, A1, and A2 are set to GND (0v).
-address = 0x48
+#loop 5 second increments
 
-# Check if another address has been specified
-if 1 < len(sys.argv):
-	address = int(sys.argv[1], 16)
+while True:
+  #function call to read core coltage
+    vcgm = Vcgencmd()
+    output = vcgm.measure_volts("core")
+ 
+    
+   #stores variable in output
+    print(output)
+   
 
-# Print temperature
-while True:	
-# Read I2C data and calculate temperature
-  bus = smbus.SMBus(1)
-  raw = bus.read_word_data(address, 0) & 0xFFFF
-  raw = ((raw << 8) & 0xFF00) + (raw >> 8)
-  temperature = (raw / 32.0) / 8.0
-  #Push Data
-  data={"temp":temperature}
-  url = "https://us-central1-personify-c98fc.cloudfunctions.net/postDeviceData"
-  data = {
+#pushing to personify database
+    url = "https://us-central1-personify-c98fc.cloudfunctions.net/postDeviceData"
+    data = {
         "uid": "XHVfPr23F7Meg0QuwEkYFvI8Fqd2",
         "accessCode": "apple",
         "DATA": {
-        "value": temperature,
-        "type": "TEMP"
+        "value": output,
+        "type": "CORE_VOLTAGE"
         }
     }
 
-  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-  r = requests.post(url, data=json.dumps(data), headers=headers)
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
 
-  print(r.text)
-  
-  time.sleep(3)
+#status confirmation text
+    print(r.text)
+
+
+#run every 10 secs
+    time.sleep(10)
